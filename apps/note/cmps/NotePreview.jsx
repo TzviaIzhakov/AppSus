cc3e10c15f4f3d74372eef5a072479db2e93cc38
TzviaIzhakov <tzvia.izhakov@gmail.com>
2023-09-06 22:46:39 +0300

    styling the notes

--- apps/note/cmps/NotePreview.jsx
+++ apps/note/cmps/NotePreview.jsx
@@ -2,7 +2,7 @@ import { NoteImg } from '../cmps/NoteImg.jsx';
 import { NoteTodos } from '../cmps/NoteTodos.jsx';
 import { NoteTxt } from '../cmps/NoteTxt.jsx';
 
-export function NotePreview({ note }) {
+export function NotePreview({ note, onRemoveNote, onSelectNoteId }) {
   return (
     <section className="note-preview">
       <div key={note.id}>
@@ -14,6 +14,22 @@ export function NotePreview({ note }) {
           //   onChangeVal(cmp.id, val);
           // }}
         />
+        <section className="user-tools-btns hidden">
+          <button onClick={() => onRemoveNote(note.id)}>
+            <img
+              src="assets/icons-notes/delete.svg"
+              alt=""
+              className="delete-img"
+            />
+          </button>
+          <button onClick={() => onSelectNoteId(note.id)}>
+            <img
+              src="assets/icons-notes/edit.svg"
+              alt=""
+              className="update-img"
+            />
+          </button>
+        </section>
       </div>
     </section>
   );
