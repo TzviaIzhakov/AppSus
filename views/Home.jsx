598a695ea7a03dc2ee769f8309604ee7e1e68816
amirshamia <amir0988@gmail.com>
2023-09-09 21:01:05 +0300

    add welcom page

--- views/Home.jsx
+++ views/Home.jsx
@@ -1,6 +1,33 @@
 
 export function Home() {
     return <section className="home">
-        <h1>Welcome to home page!</h1>
+
+
+
+
+
+        <main>
+            <section className="hero">
+                <h1 className="animate__animated animate__fadeInDown">Welcome to App</h1>
+                <img className="horse" src="https://upload.wikimedia.org/wikipedia/commons/9/9b/THIEL_619.jpg" alt="" />
+               
+            </section>
+            <section id="learn-more" class="features">
+                <div className="feature">
+                    <h2>Amir Shamia</h2>
+                    <img className="amir-img" src="https://ca.slack-edge.com/T057RE2PDLK-U05GJ575G84-5bb6aa270c01-512" alt="" />
+                </div>
+                <div className="feature">
+                    <h2>Tzvia Izhakov</h2>
+                   <img className="our-img" src="https://ca.slack-edge.com/T057RE2PDLK-U05B8H5RSF3-6ac63c88a569-512" alt="" />
+                </div>
+            
+            </section>
+        </main>
+        <footer>
+            <p>&copy; 2023 Your Website. All rights reserved.</p>
+        </footer>
+
+
     </section>
 }
